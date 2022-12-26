1bef6a03df5036128f52583341d5bfebaace225d
Florian Reinhold <kani@kemomi.de>
2022-12-26 09:49:39 +0100

    Add info logging

--- logger.go
+++ logger.go
@@ -8,6 +8,7 @@ import (
 // Logger supports trace for maximal information, debug for general debug information and error in case of an error.
 type Logger interface {
 	Errorf(format string, v ...interface{})
+	Infof(format string, v ...interface{})
 	Warnf(format string, v ...interface{})
 	Debugf(format string, v ...interface{})
 }
@@ -20,17 +21,22 @@ func NewBuiltinLogger() *BuiltinLogger {
 	return &BuiltinLogger{logger: log.New(os.Stdout, "", 5)}
 }
 
-func (l *BuiltinLogger) Debugf(format string, args ...interface{}) {
+func (l BuiltinLogger) Debugf(format string, args ...interface{}) {
 	l.logger.Print("[Debug]")
 	l.logger.Printf(format, args...)
 }
 
-func (l *BuiltinLogger) Errorf(format string, args ...interface{}) {
+func (l BuiltinLogger) Errorf(format string, args ...interface{}) {
 	l.logger.Print("[Error]")
 	l.logger.Printf(format, args...)
 }
 
-func (l *BuiltinLogger) Warnf(format string, args ...interface{}) {
+func (l BuiltinLogger) Warnf(format string, args ...interface{}) {
 	l.logger.Print("[Warn]")
 	l.logger.Printf(format, args...)
+}
+
+func (l BuiltinLogger) Infof(format string, args ...interface{}) {
+	l.logger.Print("[Info]")
+	l.logger.Printf(format, args)
 }
