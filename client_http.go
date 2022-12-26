944c0aced06ed6088ccb1fcfc7e65d3471ef9d64
Florian Reinhold <kani@kemomi.de>
2022-12-26 09:50:54 +0100

    Renamed Client -> HttpClient

--- client_http.go
+++ client_http.go
@@ -11,55 +11,55 @@ type HttpClientAdapter[R any] interface {
 	Delete(path string, container interface{}) (R, error)
 }
 
-type Client[R any] struct {
+type HttpClient[R any] struct {
 	baseUrl    string
 	httpClient HttpClientAdapter[R]
 }
 
 // CreateClient that can access the Deconz API it uses [R] as response type from http requests.
-func CreateClient[R any](httpClient HttpClientAdapter[R], settings Settings) Client[R] {
+func CreateClient[R any](httpClient HttpClientAdapter[R], settings Settings) HttpClient[R] {
 	if settings.HttpProtocol == "" {
 		settings.HttpProtocol = "http"
 	}
-	client := Client[R]{
+	client := HttpClient[R]{
 		baseUrl:    fmt.Sprintf("%s://%s/api/%s/", settings.HttpProtocol, settings.Address, settings.ApiKey),
 		httpClient: httpClient,
 	}
 	return client
 }
 
-func (c *Client[R]) getPath(path string, arguments []any) string {
+func (c *HttpClient[R]) getPath(path string, arguments []any) string {
 	path = fmt.Sprintf(path, arguments...)
 	url := c.baseUrl + path
 	return url
 }
 
-func (c *Client[R]) Get(path string, container interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) Get(path string, container interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Get(p, container)
 }
 
-func (c *Client[R]) PostWithResult(path string, body interface{}, container interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) PostWithResult(path string, body interface{}, container interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Post(p, body, container)
 }
 
-func (c *Client[R]) Post(path string, body interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) Post(path string, body interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Post(p, body, nil)
 }
 
-func (c *Client[R]) PutWithResult(path string, body interface{}, container interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) PutWithResult(path string, body interface{}, container interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Put(p, body, container)
 }
 
-func (c *Client[R]) Put(path string, body interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) Put(path string, body interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Put(p, body, nil)
 }
 
-func (c *Client[R]) Delete(path string, container interface{}, pathArguments ...any) (R, error) {
+func (c *HttpClient[R]) Delete(path string, container interface{}, pathArguments ...any) (R, error) {
 	p := c.getPath(path, pathArguments)
 	return c.httpClient.Delete(p, container)
 }
